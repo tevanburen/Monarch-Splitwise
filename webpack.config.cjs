f2ea979ab469ae0c83f0130409f5ff548b9b9cf2
tevanburen <75276166+tevanburen@users.noreply.github.com>
2025-05-01 23:49:13 -0400

    ts

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,10 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    widget: './src/widget/index.js',
+    widget: './src/widget/index.ts',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts'],
   },
   mode: 'none',
 };
