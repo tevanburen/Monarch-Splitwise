8fdd5c731f11cc172316fc8e4c491e9e89935e75
tevanburen <75276166+tevanburen@users.noreply.github.com>
2025-05-03 01:04:10 -0400

    mjs webpack

---
+++ webpack.config.mjs
@@ -0,0 +1,35 @@
+ 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: {
+    app: './src/index.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  performance: {
+    hints: false,
+  },
+  output: {
+    filename: './[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+};
