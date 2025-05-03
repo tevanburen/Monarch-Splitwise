58ca063ab20da500e4239a0471c5d5cc46288978
tevanburen <75276166+tevanburen@users.noreply.github.com>
2025-05-02 20:22:46 -0400

    remove a couple

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
@@ -6,7 +5,6 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
   { ignores: ['dist/'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
