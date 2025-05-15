12aa76906f195ff9179ac02e04be24092a7946af
tevanburen <75276166+tevanburen@users.noreply.github.com>
2025-05-14 23:54:57 -0400

    fetch

--- src/api/api.methods.ts
+++ src/api/api.methods.ts
@@ -1,15 +1,13 @@
 export const fetchMonarchRows = async (authToken: string) => {
-  console.log(authToken);
-  // In Chrome, try injecting code directly into the page like this:
-  // console.log(window);
-  // const tmp = fetch('https://api.monarchmoney.com/download-transactions/', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: AUTH_TOKEN,
-  //   },
-  //   body: JSON.stringify({accounts: ["208836640834093863"]}),
-  //   referrerPolicy: 'no-referrer',
-  // })
-  // console.log(await tmp);
+  const response = await fetch('https://api.monarchmoney.com/download-transactions/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: authToken,
+    },
+    body: JSON.stringify({accounts: ["208836640834093863"]}),
+    referrerPolicy: 'no-referrer',
+  })
+  const csv = await response.text();
+  console.log(csv);
 };
