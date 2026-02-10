3b0f7e6c3fba6e5e3812561c8061dfef425c0ab0
tevanburen <75276166+tevanburen@users.noreply.github.com>
2026-02-09 23:00:51 -0500

    colors and some other

--- src/components/NewWidget.tsx
+++ src/components/NewWidget.tsx
@@ -1,27 +1,45 @@
-import { Card, CardHeader, CardTitle } from "@/components/shadcn/card";
+import { RefreshCw, Settings } from "lucide-react";
+import { Button } from "@/components/shadcn/button";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+} from "@/components/shadcn/card";
+import { useRuntimeStateContext } from "@/providers";
 
 export const NewWidget = () => {
+	const { status, updateSingleTempState } = useRuntimeStateContext();
+
+	const isEditing = status === "editing";
+	const isRunning = status === "running";
+
 	return (
-		<Card className="p-2">
+		<Card className="p-2 gap-2">
 			<CardHeader className="p-0 gap-0">
-				{/* I don't know why the following line needs pr-2 */}
+				{/* I don't know why the following line needs pr-2, but it works */}
 				<CardTitle className="text-xl whitespace-nowrap pr-2">
-					<span className="text-orange-500">Monarch</span>
+					<span className="text-primary">Monarch</span>
 					{" - "}
-					<span className="text-green-500">Splitwise</span>
+					<span className="text-secondary">Splitwise</span>
 				</CardTitle>
 			</CardHeader>
-			{/* <CardContent className="bg-pink-400">
-				<div className="flex gap-2 w-fit mx-auto bg-yellow-300">
-					<Button variant="outline" size="icon">
-						<Settings size={18} />
-					</Button>
-					<Button className="gap-2 flex-1">
-						<RefreshCw size={18} />
-						Sync
-					</Button>
-				</div>
-			</CardContent> */}
+			<CardContent className="flex gap-2 p-0">
+				<Button
+					size="icon"
+					variant="outline"
+					className="text-secondary"
+					onClick={() =>
+						updateSingleTempState("status", isEditing ? "idle" : "editing")
+					}
+				>
+					<Settings className={isEditing ? "animate-spin" : ""} />
+				</Button>
+				<Button variant="outline" className="gap-2 flex-1 text-primary">
+					<RefreshCw className={isRunning ? "animate-spin" : ""} />
+					Sync
+				</Button>
+			</CardContent>
 		</Card>
 	);
 };
